1d728e11581a63193c615097d2d5e81a804170dd
Notsosof1 <cawhitlatch2@gmail.com>
2015-11-20 16:52:54 -0800

    Initial Commit

---
+++ Star Rating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Star Rating
+//
+//  Created by Cynthia Whitlatch on 11/20/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
